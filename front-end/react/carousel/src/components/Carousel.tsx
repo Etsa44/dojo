f414c02a382fa66163e5a120c8205b1dc28ee8f6
etsa kambandji <kmjetsa@gmail.com>
2026-08-27 21:57:04 +0200

    front-end(carousel): add circular feacture

--- front-end/react/carousel/src/components/Carousel.tsx
+++ front-end/react/carousel/src/components/Carousel.tsx
@@ -12,19 +12,27 @@ interface ImageData {
 }
 export default function Carousel({ images }: CarouselProps) {
   const [position, setPosition] = useState(0);
-  const right = () => {
-    setPosition(position + 1);
+  const next = () => {
+    if (images.length - 1 === position) {
+      setPosition(0);
+    } else {
+      setPosition(position + 1);
+    }
   };
-  const left = () => {
-    setPosition(position - 1);
+  const back = () => {
+    if (position === 0) {
+      setPosition(position + images.length - 1);
+    } else {
+      setPosition(position - 1);
+    }
   };
-  let first = images[position];
-
+  const first = images[position];
+  console.log(position);
   return (
     <div className="flex justify-center items-center">
-      <Button onClick={left} children={<ChevronLeft className="size-12" />} />
+      <Button onClick={back} children={<ChevronLeft className="size-12" />} />
       <img className="size-60" src={first.imageUrl} alt="" />
-      <Button onClick={right} children={<ChevronRight className="size-12" />} />
+      <Button onClick={next} children={<ChevronRight className="size-12" />} />
     </div>
   );
 }
