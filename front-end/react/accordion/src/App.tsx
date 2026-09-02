001066a8252138fc444af76e16de85305fb19371
etsa kambandji <kmjetsa@gmail.com>
2026-09-02 21:52:24 +0200

    front-end(accordion): first version

--- front-end/react/accordion/src/App.tsx
+++ front-end/react/accordion/src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 function App() {
-  return <div>etsa</div>;
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    if (!open) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  };
+  console.log(open);
+  return (
+    <div>
+      <button onClick={handleClick}>accordion</button>
+      {open && (
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque in
+          facere ipsam dolor quos. Nihil earum magni accusantium qui sit quo
+          labore facilis dolorum eveniet quisquam expedita est, consectetur
+          tempora.
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default App;
